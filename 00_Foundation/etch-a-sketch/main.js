ae2e017136bfeb13c61027c275e7326569e9af9d
huyennguyen <huyen.nguyen.0194@gmail.com>
2023-04-01 10:12:49 -0500

    add active state for erase button

--- 00_Foundation/etch-a-sketch/main.js
+++ 00_Foundation/etch-a-sketch/main.js
@@ -3,7 +3,8 @@ var values = {
   grid_num: 16,
   color:  "#475569",
   grid_initial_clr: "#fafafa",
-  color_choice: "solid_color"
+  color_choice: "solid_color",
+  erase: false
 }
 
 // functions 
@@ -18,20 +19,23 @@ const update_color_picker = clr => {
 
 function update_small_grid_color(e){
   let color = values.color;
-  if(values.color_choice !== "solid_color"){
+  if (values.erase){
+    color = values.grid_initial_clr;
+  }
+  else if(values.color_choice !== "solid_color" && values.erase === false ){
     color = "#"+Math.floor(Math.random()*16777215).toString(16);
   }
   e.target.style.cssText = `background:${color}`;
 }
+const clear_grid = () => {
+  grid_div.innerHTML = "";
+}
 
 const create_grid = num => {
-  
   grid_div.style.cssText = `display:grid; grid-template:repeat(${num}, 1fr)/repeat(${num}, 1fr); background-color:${values.grid_initial_clr}`
 }
 
 const add_square_to_grid = num => {
-  grid_div.innerHTML = "";
-  console.log('grid', num)
   for (let i=0; i<num*num;i++){
     const square = document.createElement("div");
     square.classList.add("square");
@@ -40,14 +44,20 @@ const add_square_to_grid = num => {
   }
 }
 
+const reset_grid = () => {
+  clear_grid();
+  create_grid(values.grid_num);
+  add_square_to_grid(values.grid_num);
+}
+
 const reset = () => {
   values.grid_num = 16;
   values.color = "#475569"
-  values.color_choice = "solid_color"
-  create_grid(values.grid_num);
-  add_square_to_grid(values.grid_num);
+  values.color_choice = "solid_color";
+  values.erase = false;
   display_range(values.grid_num);
   update_color_picker(values.color);
+  reset_grid();
 }
 
 // declar valirable
@@ -58,35 +68,40 @@ const color_picker = document.getElementById("color-picker");
 const reset_btn = document.getElementById("reset-btn");
 const erase_btn = document.getElementById("erase-btn");
 const random_clr_btn = document.getElementById("random-clr-btn");
-// const squares = document.querySelectorAll('.square');
 
 //when user select random color:
 random_clr_btn.addEventListener('click', (e) => {
   values.color_choice = "random";
 });
+
 // when user hits reset
 reset_btn.addEventListener('click', reset );
+
 // when user erase
 erase_btn.addEventListener('click', (e) => {
-  console.log(e.target)
+  erase_btn.classList.toggle('active');
+  values.erase = !values.erase;
 })
+
 // when user change color 
-color_picker.addEventListener('change', event => {
+color_picker.addEventListener('click', event => {
   values.color = event.target.value;
-  console.log(values)
+  values.color_choice = "solid_color"
 })
 //when user changes grid range size
 range_picker.addEventListener('mousemove', (e) => {
   values.grid_num = e.target.value;
-  display_range(values.grid_num);
+  reset_grid();
+  display_range(e.target.value);
 })
 
 range_picker.addEventListener('change', (e) => {
   values.grid_num = e.target.value;
-  create_grid(values.grid_num);
-  add_square_to_grid(values.grid_num);
+  reset_grid();
 })
 
+// initialize the page
 window.onload = () => {
   reset();
 }
+
