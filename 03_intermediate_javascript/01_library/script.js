86db07e407986c17efc8f9fc219ce8f90d2f7947
huyennguyen <huyen.nguyen.0194@gmail.com>
2023-04-04 00:46:31 -0500

    add storage for library

--- 03_intermediate_javascript/01_library/script.js
+++ 03_intermediate_javascript/01_library/script.js
@@ -6,51 +6,40 @@ const add_book_btn = document.getElementById("add-book-btn");
 const close_form_btn = document.getElementById("close-modal-btn");
 const modal = document.getElementById("book-form-modal");
 
-let library = [
-  new Book("no author", "Once upon a time", "12-01-2001", "23", false),
-];
-
-// function
-function Book(author, title, publication_date, page_no, read_status) {
+var library = [];
+function Book(author, title, publication_date, page_no, read_status, book_id) {
   this.author = author;
   this.title = title;
   this.publication_date = publication_date;
   this.page_no = page_no;
   this.read_status = read_status;
-  this._id = 1;
+  book_id !== null ? (this._id = book_id) : (this._id = 1);
   this.set_id = function (id) {
     this._id = id;
   };
 }
 
 const get_random_color = () =>
-  ["#fdba74", "#bbf7d0", "#ddd6fe", "#fecdd3", "#93c5fd", "#fca5a5"][
-    Math.floor(Math.random() * 6)
+  ["#fdba74", "#bbf7d0", "#ddd6fe", "#fecdd3", "#93c5fd", "#fca5a5", "#fef08a"][
+    Math.floor(Math.random() * 7)
   ];
-
-const toggle_add_book_form = (display_form) => {
-  display_form
-    ? book_form_section.classList.remove("hidden")
-    : book_form_section.classList.add("hidden");
-};
 
 const display_book = (book) => {
   const card_div = document.createElement("div");
   card_div.classList.add("book");
   card_div.classList.add("card");
-
+  card_div.dataset.id = book._id;
   const read_status = book.read_status ? "checked" : "";
   const html_content = `
   <div class="card-content" >
-    <div class="card-del">
-      <button onClick="delete_book(${book._id})" id="del-btn">X</button>
-    </div>
+    <button onClick="delete_book(${book._id})" id="del-btn">X</button>
+
     <div class="title"><span>${book.title}</span></div>
     <div class="author">
       <span>By: </span><span class="book-label">${book.author}</span>
     </div>
     <div class="date">
-      <span>Published:</span><span class="book-label">${book.publication_date}</span>
+      <span>Published: </span><span class="book-label">${book.publication_date}</span>
     </div>
     <div class="page-no">
       <span>Number of pages: </span><span class="book-label">${book.page_no}</span>
@@ -58,7 +47,7 @@ const display_book = (book) => {
   </div>
 
   <label class="toggle">
-      <input class="toggle-input" type="checkbox" ${read_status}/>
+      <input  onClick="toggle_read_status(${book._id})" class="toggle-input" type="checkbox" ${read_status}/>
       <span class="toggle-label" data-off="NOT READ" data-on="READ"></span>
       <span class="toggle-handle"></span>
   </label>
@@ -67,11 +56,6 @@ const display_book = (book) => {
 
   card_div.innerHTML = html_content;
   container.insertBefore(card_div, add_book_div);
-
-  const toggle_btn = document.querySelector(".toggle-input");
-  toggle_btn.onclick = (e) => {
-    book.read_status = e.target.checked;
-  };
 };
 
 const display_books = (books) => {
@@ -83,12 +67,12 @@ const display_books = (books) => {
 const add_book_to_library = (book) => {
   library.push(book);
   display_book(book);
+  window.localStorage.setItem("myLibrary", JSON.stringify(library));
 };
 
 const form = document.querySelector("form");
 form.onsubmit = (event) => {
   event.preventDefault();
-
   const title = document.getElementById("book-title");
   const author = document.getElementById("book-author");
   const publication_date = document.getElementById("book-published-date");
@@ -107,6 +91,7 @@ form.onsubmit = (event) => {
   form.reset();
 };
 
+// user click show add book form
 show_book_form_btn.onclick = () => modal.classList.remove("hidden");
 close_form_btn.onclick = () => modal.classList.add("hidden");
 window.onclick = (e) => {
@@ -119,7 +104,35 @@ const delete_book = (book_id) => {
   if (del) {
     library = library.filter((book) => book._id !== book_id);
     display_books(library);
+    window.localStorage.setItem("myLibrary", JSON.stringify(library));
   }
 };
 
-window.onload = () => display_books(library);
+// user toggle rad
+const toggle_read_status = (id) => {
+  library.forEach((lib_book) => {
+    lib_book.read_status =
+      lib_book._id === id ? !lib_book.read_status : lib_book.read_status;
+  });
+  window.localStorage.setItem("myLibrary", JSON.stringify(library));
+};
+
+window.onload = () => {
+  const storage = JSON.parse(window.localStorage.getItem("myLibrary"));
+  console.log(storage);
+  if (storage !== null) {
+    library = storage.map(
+      (book) =>
+        new Book(
+          book.author,
+          book.title,
+          book.publication_date,
+          book.page_no,
+          book.read_status,
+          book._id
+        )
+    );
+
+    display_books(library);
+  }
+};
