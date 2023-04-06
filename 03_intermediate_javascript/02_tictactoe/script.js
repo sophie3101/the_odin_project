d5dcb53aed22af69e5e52c00f23dbc4254ee987a
huyennguyen <huyen.nguyen.0194@gmail.com>
2023-04-06 17:59:44 -0500

    change toggle selection to buttons

--- 03_intermediate_javascript/02_tictactoe/script.js
+++ 03_intermediate_javascript/02_tictactoe/script.js
@@ -1,9 +1,11 @@
-const Player = function (name, markType) {
+const Player = function (name, markType, type) {
   let status = false; //false for inactive and true for active
   score = 0;
   return {
     name,
     markType,
+    score,
+    type,
     status,
     updateStatus() {
       this.status = !status;
@@ -61,6 +63,7 @@ const GameBoard = () => {
         if (cell === "") total++;
       })
     );
+
     return total;
   };
 
@@ -103,7 +106,7 @@ const GameBoard = () => {
       });
       has_winner = diagonal_sum.indexOf(3) !== -1;
     }
-    console.log(has_winner);
+
     return has_winner;
   };
 
@@ -129,6 +132,7 @@ const GameController = (...players) => {
   const getActivePlayerMark = () => players[playerActiveIdx].markType;
 
   const playerCheckMark = (rowIndex, colIndex) => {
+    console.log("playActiveidx", playerActiveIdx);
     const currentPlayer = players[playerActiveIdx];
     gameBoard.updateBoard(rowIndex, colIndex, currentPlayer.markType);
     currentPlayer.updateStatus();
@@ -137,21 +141,34 @@ const GameController = (...players) => {
     // switch to other player
     playerActiveIdx = playerActiveIdx === 1 ? 0 : 1;
 
-    console.log("playerIdx after switch", playerActiveIdx);
+    console.log("currentPlayer idx ", playerActiveIdx);
   };
 
-  const canGameEnd = () => gameBoard.countEmptyCell === 0;
+  const canGameEnd = () => gameBoard.countEmptyCell() === 0;
+
   const otherPlayerPlay = () => {
     randomIndices = gameBoard.getRandomEmptyCell();
     const activePlayerMark = getActivePlayerMark();
-    console.log("random cell: ", randomIndices);
+    // console.log("random cell: ", randomIndices);
     playerCheckMark(...randomIndices);
     return { indices: randomIndices, markType: activePlayerMark };
   };
 
   // const restartGame = () => (gameBoard = GameBoard());
   const restartGame = () => gameBoard.initializeBoard();
+
   const hasWinner = (markType) => gameBoard.checkBoard(markType);
+
+  const getWinnerName = (markType) => {
+    const winnerPlayer = players.find((player) => player.markType === markType);
+    const winnerName = winnerPlayer.name;
+
+    // update score for winner
+    winnerPlayer.updateScore();
+
+    return winnerName;
+  };
+
   return {
     playerCheckMark,
     getActivePlayerIdx,
@@ -160,42 +177,50 @@ const GameController = (...players) => {
     restartGame,
     hasWinner,
     canGameEnd,
+    getWinnerName,
   };
 };
 
 (function (document) {
-  const starterSection = document.getElementById("section-starter");
   const userAndMachineBtn = document.getElementById("user-vs-machine");
   const userAndUser = document.getElementById("user-vs-user");
-  const signs = document.getElementsByName("user-sign-selection");
-  //game
+  const starterSection = document.getElementById("section-starter");
   const gameSection = document.getElementById("game-section");
-  const userScoreDiv = document.getElementById("display-user");
-  const otherUserScoreDiv = document.getElementById("display-other-user");
-  const resetBtn = document.getElementById("reset-btn");
-  const turnDisplayDiv = document.getElementById("turn-update");
+
   let gameController;
 
+  const signsBtn = document.querySelectorAll("button[type='button']");
+  console.log(signsBtn);
   // game
+  const userScoreDiv = document.getElementById("display-user");
+  const otherUserScoreDiv = document.getElementById("display-other-user");
+  const tieScoreP = document.getElementById("ties-score");
   const cells = document.querySelectorAll(".cell");
 
-  //
   let userSignSelection = "";
-  // in firefox the radio button is automatically checked
-  signs.forEach((sign) => {
-    if (sign.checked) userSignSelection = sign.id;
-  });
 
-  signs.forEach(
+  signsBtn.forEach(
     (sign) =>
       (sign.onclick = (e) => {
-        userSignSelection = sign.id;
+        console.log(e);
+        console.log(sign.dataset.icon);
+        userSignSelection = sign.dataset.icon;
       })
   );
 
   userAndMachineBtn.onclick = (e) => displayGame(e);
 
   userAndUser.onclick = (e) => displayGame(e);
+
+  const showHomeScreen = () => {
+    gameSection.classList.add("disabled");
+    starterSection.classList.remove("disabled");
+  };
+
+  const showGameScreen = () => {
+    starterSection.classList.add("disabled");
+    gameSection.classList.remove("disabled");
+  };
 
   const updateScreen = (indices, markType) => {
     // console.log("marktype", markType, indices);
@@ -212,37 +237,78 @@ const GameController = (...players) => {
     });
   };
 
-  const updateTurn = (markType) => (turnDisplayDiv.innerText = `${markType}`);
-
-  const displayQuestion = () => {
-    // ask user if they want to continue the game or not
+  const updateTurn = (markType) => {
+    const turnDisplayDiv = document.getElementById("turn-update-content");
+    turnDisplayDiv.innerText = `${markType}`;
   };
-  const displayGame = (e) => {
-    console.log("user select sign: ", userSignSelection);
-    if (userSignSelection === "sign-x") {
-      userScoreDiv.classList.add("x");
-      otherUserScoreDiv.classList.add("o");
-    } else {
-      userScoreDiv.classList.add("o");
-      otherUserScoreDiv.classList.add("x");
-    }
-    const userMarkSign = userSignSelection === "sign-x" ? "X" : "O";
 
-    const userPlayer = Player("user", userMarkSign);
-    const otherSign = userMarkSign === "X" ? "O" : "X";
-    let otherPlayer;
-    if (e.target === userAndMachineBtn) {
-      otherPlayer = Player("machine", otherSign);
-    } else {
-      otherPlayer = Player("otherUser", otherSign);
-    }
+  const createElement = (elementType, text, idName) => {
+    let ele = document.getElementById(idName);
+    if (ele === null) ele = document.createElement(elementType);
+
+    if (idName.length !== 0) ele.setAttribute("id", idName);
+    ele.innerHTML = text;
+
+    return ele;
+  };
+
+  const displayGame = (e) => {
+    if (userSignSelection === "") return;
+    console.log("user select sign: ", userSignSelection);
+    const userMarkSign = userSignSelection.toUpperCase();
+    const otherUserMarkSign = userMarkSign === "X" ? "O" : "X";
+    const userPlayerName = "YOU";
+    const userPlayer = Player(userPlayerName, userMarkSign, "human");
+
+    const otherPlayerName = "OTHER USER";
+    let otherPlayerType;
+    if (e.target === userAndMachineBtn) otherPlayerType = "machine";
+    else otherPlayerType = "human";
+
+    const otherPlayer = Player(
+      otherPlayerName,
+      otherUserMarkSign,
+      otherPlayerType
+    );
     console.log(userPlayer, otherPlayer);
-    starterSection.classList.add("disabled");
-    gameSection.classList.remove("disabled");
+    // append color, text and class for result display div
+    userScoreDiv.classList.add(`${userMarkSign.toLowerCase()}`);
+    userScoreDiv.appendChild(
+      createElement(
+        "p",
+        `${userMarkSign}(${userPlayerName})`,
+        `${userPlayerName}-name`
+      )
+    );
+    userScoreDiv.appendChild(
+      createElement("p", userPlayer.score, `${userPlayer.name}-score`)
+    );
+
+    otherUserScoreDiv.classList.add(`${otherUserMarkSign.toLowerCase()}`);
+    const otherUserDisplayName =
+      otherPlayerType === "human"
+        ? `${otherUserMarkSign}( ${otherPlayerName})`
+        : `${otherUserMarkSign}( CPU)`;
+
+    otherUserScoreDiv.appendChild(
+      createElement("p", otherUserDisplayName, `${otherPlayerName}-name`)
+    );
+    otherUserScoreDiv.appendChild(
+      createElement("p", otherPlayer.score, `${otherPlayerName}-score`)
+    );
+
+    // hide starter section
+    showGameScreen();
 
     //start game
-
     startGame(userPlayer, otherPlayer);
+  };
+
+  const machinePlay = (gameController) => {
+    let { indices, markType } = gameController.otherPlayerPlay();
+    updateScreen(indices, markType);
+
+    return markType;
   };
 
   const startGame = (userPlayer, otherPlayer) => {
@@ -250,29 +316,80 @@ const GameController = (...players) => {
     let activePlayerIdx = gameController.getActivePlayerIdx();
     console.log("active player idx: ", activePlayerIdx);
     updateTurn(gameController.getActivePlayerMark());
-    //start Case 1: user vs machine
-    // if (activePlayerIdx === 1) {
-    //   let { indices, markType } = gameController.otherPlayerPlay();
-    //   updateScreen(indices, markType);
-    // }
-
+    let markType;
     // start case 1: user vs another user
     cells.forEach(
       (cell) =>
         (cell.onclick = (e) => {
+          if (e.target.textContent !== "") return;
           const rowIdx = Number(e.target.dataset.row) - 1;
           const colIdx = Number(e.target.dataset.col) - 1;
-          const markType = gameController.getActivePlayerMark();
+          markType = gameController.getActivePlayerMark();
+          console.log("user mark type ", markType);
           gameController.playerCheckMark(rowIdx, colIdx);
           updateScreen([rowIdx, colIdx], markType);
-          if (gameController.hasWinner(markType)) {
+
+          if (otherPlayer.type === "machine" && !gameController.canGameEnd()) {
+            setTimeout(() => {
+              markType = machinePlay(gameController);
+            }, 600);
           }
-          if (gameController) updateTurn(gameController.getActivePlayerMark());
+
+          if (gameController.hasWinner(markType)) {
+            const winnerName = gameController.getWinnerName(markType);
+            console.log(userPlayer, otherPlayer, winnerName);
+            showScores(userPlayer, otherPlayer);
+            showDialog(`${winnerName} wins!!!`);
+          } else if (gameController.canGameEnd()) {
+            showScores(userPlayer, otherPlayer, true);
+            showDialog("TIE");
+          }
+          updateTurn(gameController.getActivePlayerMark());
         })
     );
+
+    //start Case 2: user vs MACHINE
+    if (activePlayerIdx === 1 && otherPlayer.type === "machine")
+      machinePlay(gameController);
   };
 
-  resetBtn.onclick = (e) => {
+  const showScores = (userPlayer, otherPlayer, tie = false) => {
+    const userScoreP = document.getElementById(`${userPlayer.name}-score`);
+    userScoreP.innerText = userPlayer.score;
+
+    const otherScoreP = document.getElementById(`${otherPlayer.name}-score`);
+    otherScoreP.innerText = otherPlayer.score;
+
+    // if tie
+    tie
+      ? (tieScoreP.textContent = Number(tieScoreP.textContent) + 1)
+      : tieScoreP.textContent;
+  };
+
+  const showDialog = (displayContent) => {
+    const dialog = document.getElementById("dialog");
+    const dialogHeader = document.getElementById("dialog-header");
+    dialogHeader.innerText = displayContent;
+    dialog.showModal();
+
+    const restartBtn = document.getElementById("restart-btn");
+    restartBtn.onclick = (e) => {
+      console.log(e);
+      dialog.close();
+      resetGame();
+      tieScoreP.innerText = 0;
+      showHomeScreen();
+    };
+
+    const nextBtn = document.getElementById("next-round-btn");
+    nextBtn.onclick = (e) => {
+      console.log(e);
+      resetGame();
+      dialog.close();
+    };
+  };
+
+  const resetGame = () => {
     gameController.restartGame();
     const cellMarks = document.querySelectorAll('div[class^="cell-mark-"]');
     cellMarks.forEach((cell) => {
@@ -283,4 +400,7 @@ const GameController = (...players) => {
       .querySelectorAll(".disabled-pointer")
       .forEach((div) => div.classList.remove("disabled-pointer"));
   };
+
+  const resetBtn = document.getElementById("reset-btn");
+  resetBtn.onclick = () => resetGame();
 })(document);
